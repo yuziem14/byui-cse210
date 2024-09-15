68a21fc3c8cdd087897941d3bdc49c840a870022
Yuri Ziemba <yuri.fnunes@gmail.com>
2024-09-15 18:24:47 -0300

    prep4 with lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            numbers.Add(number);
+        } while(number != 0);
+        /** Remove 0 from the end of the list */
+        numbers.RemoveAt(numbers.Count - 1);
+        numbers.Sort();
+
+        int smallestPositiveNumber = -1;
+
+        for(int i = 0; i < numbers.Count; i++) {
+            if(smallestPositiveNumber == -1 && numbers[i] >= 0) {
+                smallestPositiveNumber = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"The sum is {numbers.Sum()}");
+        Console.WriteLine($"The average is {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers[numbers.Count - 1]}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        Console.WriteLine("The sorted list is:");
+        for(int i = 0; i < numbers.Count; i++) {
+            Console.WriteLine(numbers[i]);
+        }
     }
 }
