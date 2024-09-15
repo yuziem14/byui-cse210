339233399d9f67d10186377cde075b4bde3d8d5a
Yuri Ziemba <yuri.fnunes@gmail.com>
2024-09-15 17:37:34 -0300

    prep 2 with conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        int lastDigit = gradePercentage % 10;
+
+        string letter = "A";
+        string signal = "";
+        string message = "You passed!";
+
+        if(gradePercentage < 60) {
+            letter = "F";
+        } 
+        else if(gradePercentage >= 60 && gradePercentage < 70) {
+            letter = "D";
+        }
+        else if(gradePercentage >= 70 && gradePercentage < 80) {
+            letter = "C";
+        } else if(gradePercentage >= 80 && gradePercentage < 90) {
+            letter = "B";
+        }
+
+        if(lastDigit < 3) {
+            signal = "-";
+        } else if(lastDigit >= 7) {
+            signal = "+";
+        }
+        
+        /** If grade is 'F' or it would be 'A+' dont set signal */
+        if(gradePercentage < 60 || gradePercentage > 96) {
+            signal = "";
+        }
+
+        if(gradePercentage < 70) {
+            message = "Better luck next time!";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{signal}");
+        Console.WriteLine(message);
     }
 }
