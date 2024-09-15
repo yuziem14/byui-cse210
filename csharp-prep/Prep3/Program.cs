3575257371a82ab2fb55b6341c7bac1fffffc695
Yuri Ziemba <yuri.fnunes@gmail.com>
2024-09-15 17:53:05 -0300

    prep 3 with loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int attempts = 0;
+        int userGuessNumber;
+        string continuePlaying;
+
+        do {
+            Console.Write("What is your guess? ");
+            userGuessNumber = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if(userGuessNumber < magicNumber) {
+                Console.WriteLine("Higher");
+            } else if(userGuessNumber > magicNumber) {
+                Console.WriteLine("Lower");
+            } else {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"Number of Attempts: {attempts}");
+
+                /** Check if user wants to play again. If yes, it will generate a new magic number, reset attempts and change guess number to not finish the loop */
+                Console.WriteLine("Play again (yes)?");
+                continuePlaying = Console.ReadLine();
+
+                if(continuePlaying == "yes") {
+                    magicNumber = randomGenerator.Next(0, 100);
+                    userGuessNumber = -1;
+                    attempts = 0;
+                }
+            }
+
+        } while(magicNumber != userGuessNumber);
     }
 }
